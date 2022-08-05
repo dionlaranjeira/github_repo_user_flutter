d4874102e2615b82a484136e88dbd924e23caae1
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-08-05 12:57:09 -0400

    feat: return List<Users> from viewmodel

---
+++ lib/view_model/list_users.dart
@@ -0,0 +1,20 @@
+import 'package:github_repo_user/model/user.dart';
+import 'package:github_repo_user/services/github_api.dart';
+
+class ListUsersViewModel{
+  final GitHubApi _api = GitHubApi();
+
+  List<User>? users;
+  Future<void> getRandomUsers() async {
+    final apiResult = await _api.getRandomUsers();
+
+    List<User> listUsers = [];
+
+    for(var user in apiResult){
+      User u = User.fromJson(user);
+      listUsers.add(u);
+    }
+    users = listUsers;
+  }
+
+}
