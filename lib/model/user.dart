67cd0ac0c3ac698f780bbbc004f2d9d4ea082226
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-08-05 12:31:43 -0400

    test: Shold return an List<User>

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -66,6 +66,12 @@ class User {
       this.createdAt,
       this.updatedAt});
 
+
+  @override
+  String toString() {
+    return 'User{login: $login, id: $id, nodeId: $nodeId, avatarUrl: $avatarUrl, gravatarId: $gravatarId, url: $url, htmlUrl: $htmlUrl, followersUrl: $followersUrl, followingUrl: $followingUrl, gistsUrl: $gistsUrl, starredUrl: $starredUrl, subscriptionsUrl: $subscriptionsUrl, organizationsUrl: $organizationsUrl, reposUrl: $reposUrl, eventsUrl: $eventsUrl, receivedEventsUrl: $receivedEventsUrl, type: $type, siteAdmin: $siteAdmin, name: $name, company: $company, blog: $blog, location: $location, email: $email, hireable: $hireable, bio: $bio, twitterUsername: $twitterUsername, publicRepos: $publicRepos, publicGists: $publicGists, followers: $followers, following: $following, createdAt: $createdAt, updatedAt: $updatedAt}';
+  }
+
   User.fromJson(Map<String, dynamic> json) {
     login = json['login'];
     id = json['id'];
--- test/services/github_api_test.dart
+++ test/services/github_api_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:github_repo_user/model/user.dart';
 import 'package:github_repo_user/services/github_api.dart';
 
 void main(){
@@ -7,6 +8,19 @@ void main(){
 
   test('Should call api.getRandomUsers and return List<Map<String, dynamic>>', () async {
     final List<Map<String, dynamic>> response = await api.getRandomUsers();
+  });
+
+  test("Shold return an List<User>", () async{
+
+    final response  = await api.getRandomUsers();
+
+    List<User> listUsers = [];
+
+    for(var user in response){
+      User u = User.fromJson(user);
+      listUsers.add(u);
+    }
+
   });
 
 }
