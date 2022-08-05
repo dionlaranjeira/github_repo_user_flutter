8a2b0d6b7331c70c7dedacfc5dfbf698c5bbd5d8
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-08-05 14:15:11 -0400

    refactor: move widgets initial_page.dart from methods

---
+++ lib/view/components/card_user.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:github_repo_user/model/user.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class CardUser extends StatelessWidget {
+  final User user;
+
+  const CardUser({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: null,
+      child: Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+            side: const BorderSide(color: Colors.blue, width: 1),
+            borderRadius: BorderRadius.circular(4.0)),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                user.login ?? "",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Hero(
+              tag: user.id!,
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  child: CachedNetworkImage(
+                    imageUrl: user.avatarUrl!,
+                    placeholder: (context, url) => const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(),
+                    ),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/view/initial_page.dart
+++ lib/view/initial_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:github_repo_user/model/user.dart';
+import 'package:github_repo_user/view/components/card_user.dart';
 import 'package:github_repo_user/view_model/list_users.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 class InitialPage extends StatelessWidget {
   ListUsersViewModel listUsersViewModel = ListUsersViewModel();
@@ -16,75 +16,45 @@ class InitialPage extends StatelessWidget {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
             case ConnectionState.active:
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Loading data...",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 30),
-                  Center(child: CircularProgressIndicator()),
-                ],
-              );
+              return buildColumnLoading();
             case ConnectionState.done:
-              // return buildColumn(listUsersViewModel.users!);
-              return MasonryGridView.count(
-                // controller: _scrollController,
-                itemCount: listUsersViewModel.users!.length,
-                crossAxisCount: 2,
-                itemBuilder: (context, index) {
-                  return cardUser(listUsersViewModel.users![index]);
-                },
-              );
+              return buildGridViewUsers(listUsersViewModel.users!);
             case ConnectionState.none:
-              return const Center(child: Text('Internet connection problems.'));
+              return buildInforText('Internet connection problems.');
             default:
-              return const Center(child: Text('Problems receiving data.'));
+              return buildInforText('Problems receiving data.');
           }
         },
       ),
     );
   }
 
-  InkWell cardUser(User user) {
-    return InkWell(
-      onTap: null,
-      child: Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-            side: const BorderSide(color: Colors.blue, width: 1),
-            borderRadius: BorderRadius.circular(4.0)),
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            ListTile(
-              title: Text(
-                user.login ?? "",
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Hero(
-              tag: user.id!,
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  child: CachedNetworkImage(
-                    imageUrl: user.avatarUrl!,
-                    placeholder: (context, url) => const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: CircularProgressIndicator(),
-                    ),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
+  Center buildInforText(String text) => const Center(child: Text('Internet connection problems.'));
+
+  MasonryGridView buildGridViewUsers(List<User> users) {
+    return MasonryGridView.count(
+              // controller: _scrollController,
+              itemCount: users.length,
+              crossAxisCount: 2,
+              itemBuilder: (_, index) {
+                User user = users![index];
+                return CardUser(user: user);
+              },
+            );
+  }
+
+  Column buildColumnLoading() {
+    return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  "Loading data...",
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                SizedBox(height: 30),
+                Center(child: CircularProgressIndicator()),
+              ],
+            );
   }
 }
