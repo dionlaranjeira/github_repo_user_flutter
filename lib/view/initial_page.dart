b9b7fcf1ed1abdd074fa2cb83131445809f4db61
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-08-05 13:21:07 -0400

    feat: Show users in initial_page.dart

--- lib/view/initial_page.dart
+++ lib/view/initial_page.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:github_repo_user/model/user.dart';
+import 'package:github_repo_user/view_model/list_users.dart';
 
 class InitialPage extends StatelessWidget {
 
+  ListUsersViewModel listUsersViewModel = ListUsersViewModel();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Initial page"),),
+    return Scaffold(
+      body: FutureBuilder(
+        future: listUsersViewModel.getRandomUsers(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Loading data...",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 30),
+                  Center(child: CircularProgressIndicator()),
+                ],
+              );
+            case ConnectionState.done:
+              return buildColumn(listUsersViewModel.users!);
+            case ConnectionState.none:
+              return const Center(child: Text('Internet connection problems.'));
+            default:
+              return const Center(child: Text('Problems receiving data.'));
+          }
+        },
+      ),
+    );
+  }
+
+  Column buildColumn(List<User> users) {
+    return Column(
+      children: users.map((e) => Text(e.login!)).toList(),
     );
   }
 }
--- test/services/github_api_test.dart
+++ test/services/github_api_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:github_repo_user/model/user.dart';
 import 'package:github_repo_user/services/github_api.dart';
